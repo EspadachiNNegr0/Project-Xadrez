b9b5a43900316b41e1e040b7d1e0463d13ce017c
Eduardo Ferreira <edu.santosgomesferreira@gmail.com>
2024-02-27 15:49:38 -0300

    first class: Position

---
+++ Xadrez/src/applicattion/Main.java
@@ -0,0 +1,11 @@
+package applicattion;
+
+import boardgame.position;
+
+public class Main {
+    public static void main(String[] args) {
+        position pos = new position(3, 5);
+        System.out.println(pos);
+
+    }
+}
